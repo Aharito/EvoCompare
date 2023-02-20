f8771554a10b0d8564d6bc05e97cf89e1c5bb1ee
Aharito <harand@mail.ru>
2023-02-20 23:55:47 +0700

    Update to Release 2.0.0

--- assets/snippets/Compare/config.compare.php
+++ assets/snippets/Compare/config.compare.php
@@ -6,17 +6,36 @@ $compareMaxCount = 5;
 // Класс активности
 $compareActiveClass = 'active';
 
-// Здесь задайте остальные параметры по правилам ДокЛистера, применимы все параметры ДокЛистера
-// (!) Список ниже - это пример, вы можете одни параметры удалить, вторые помеянть, а третьи добавить
+// Здесь задайте остальные параметры по правилам ДокЛистера. Применимы все параметры ДокЛистера.
+// (!) Список ниже - это пример, вы можете одни параметры удалить, вторые поменять, а третьи добавить
+// (!!) Не действует параметр tvList, так как он автоматом формируется из rowsList и requiredList
 $params = [
     'ownerTPL' => '@CODE:
         <div class="row page-block__grid mb-4">
-            [+dl.wrap+]
+            <div class="table">
+                <table>
+                    [+wrap+]
+                </table>
+            </div>
         </div>',
-    'tpl' => 'tovar',
-    'tvList' => 'dlina,shir,color,material,tekstura,plotn,price',
+    'topRowTpl' => '@CODE:<tr class="first"><td><b>Параметры<b></td>[+wrap+]</tr>',        
+    // Это пример с инлайн-шаблоном
+    'topRowItemTpl' => '@CODE:<td><a href="[+url+]"><img class="img-fluid" style="width: 200px" src="[+picture+]"></a></td>',
+    // А это пример с чанком
+    'topRowItemTpl' => '@CHUNK:topRowItemTpl',
+    'rowTpl' => '@CODE:<tr class="[+class+]"><td><small><b>[+rowName+]</b></small></td>[+wrap+]</tr>',
+    'paramTpl' => '@CODE:<td>[+value+]</td>',
+    // Список полей документа, ТВ-параметров, препаре-"полей".
+    // В том порядке, в котором надо отображать, через запятую. ТВ-параметры пишем как tv.dlina
+    'rowsList' => 'tv.nazv,tv.vid,tv.tol,tv.dlina,tv.shir,tv.color,tv.plotn,tv.razm,tv.price', 
+    // Названия для параметров из rowsList, в том же порядке, что и в rowsList, разделение верт. чертой "|"
+    'rowsNames' => 'Название|Вид|Толщина|Длина|Ширина,<br>мм|Цвет|Плотность|Размер|Цена',
+    // Доп. список величин, которые не нужны в сравнении, но нужны в шаблонах или для prepare, или для выборки
+    // Порядок не важен, ТВ-параметры также пишем как префикс с именем tv.dlina
+    'requiredList' => 'picture',
     'sortBy' => 'menuindex',
-    'sortOrder' => 'DESC',
-    'prepare' => '',
+    'sortOrder' => 'ASC',
+    'prepare' => 'Tovar_picture,Tovar_labels,Tovar_data',
+    'api' => '1',
     'debug' => 0,
 ];
