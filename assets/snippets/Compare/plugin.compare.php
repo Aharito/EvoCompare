490c32cd587a57a23067c0b0a7d106b597e75145
Aharito <harand@mail.ru>
2023-02-20 23:56:46 +0700

    Update to Release 2.0.0

--- assets/snippets/Compare/plugin.compare.php
+++ assets/snippets/Compare/plugin.compare.php
@@ -1,30 +1,93 @@
 <?php
 
+if (!class_exists('\\DLTemplate')) {
+    include_once(MODX_BASE_PATH . 'assets/snippets/DocLister/lib/DLTemplate.class.php');
+}
+$DLT = DLTemplate::getInstance($modx);
+
 $compareMaxCountMsg = [
-    'en' => 'You can only compare up to '.$compareMaxCount.' products at a time. Please remove a product from the comparison list to add a new one.',
-    'ru' => 'Вы можете сравнивать не более '.$compareMaxCount.' товаров одновременно. Пожалуйста, удалите товар из сравнения и добавьте другой.',
+    'en' => 'You can only compare up to ' . $compareMaxCount . ' products at a time. Please remove a product from the comparison list to add a new one.',
+    'ru' => 'Вы можете сравнивать не более ' . $compareMaxCount . ' товаров одновременно. Пожалуйста, удалите товар из сравнения и добавьте другой.',
 ];
 
-if($modx->event->name==='OnPageNotFound') {
+if ($modx->event->name === 'OnPageNotFound') {
     // Render Compare table with products
-    if(isset($_GET['q']) && $_GET['q'] == 'compare-list-render') {
+    if (isset($_GET['q']) && $_GET['q'] == 'compare-list-render') {
         $requiredParams = [
             'idType' => 'documents',
             'documents' => $_COOKIE['compare_list'],
             'display' => $compareMaxCount,
         ];
 
+        /**
+         * Выбираем из списков rowsList и requiredList имена с подстрокой 'tv.'
+         * и формируем из таких имен список tvList (сначала удаляя префиксы) для вызова DocLister
+         */
+        $tvPrefix = "tv.";
+        $arr['tvList'] = array();
+        $params['tvList'] = '';
+
+        foreach (['rowsList', 'requiredList'] as $listName) {
+            $params[$listName] = str_replace(' ', '', $params[$listName]);
+
+            if (!empty($params[$listName])) {
+                $arr[$listName] = explode(',', $params[$listName]);
+
+                foreach ($arr[$listName] as $paramName) {
+                    if (strpos($paramName, $tvPrefix) !== false) {
+                        $arr['tvList'][] = $paramName;
+                    }
+                }
+            }
+            $params['tvList'] = str_replace($tvPrefix, '', implode(',', $arr['tvList']));
+        }
+
         $result = $modx->runSnippet('DocLister', array_merge($requiredParams, $params));
-        echo $modx->parseDocumentSource($result);
+        $result = json_decode($result, 1);
+
+        $out = '';
+        /**
+         * Верхняя строка таблицы
+         */
+        $topRow = $ph['wrap'] = '';
+        foreach ($result as $document) {
+            $ph['wrap'] .= $DLT->parseChunk($params['topRowItemTpl'], $document);
+        }
+        $topRow = $DLT->parseChunk($params['topRowTpl'], $ph);
+
+        /**
+         * Строки в теле таблицы
+         */
+        $rows = $ph['wrap'] = '';
+        if (is_array($arr['rowsList']) && !empty($arr['rowsList'])) {
+            $arr['rowsNames'] = explode('|', $params['rowsNames']); // Имена для строк
+            $arr['rowsList'] = str_replace('.', '_', $arr['rowsList']); // В режиме АПИ у ТВ не точки, а подчеркивания
+            for ($i = 0; $i < count($arr['rowsList']); $i++) {
+                foreach ($result as $document) {
+                    //echo("Парам ".$paramName." Документ: ".$document['id']."<br>");
+                    $ph['value'] = $document[$arr['rowsList'][$i]];
+                    $ph['wrap'] .= $DLT->parseChunk($params['paramTpl'], $ph);
+                }
+                $ph['class'] = ($i % 2) ? 'odd' : 'even';
+                $ph['rowName'] =  $arr['rowsNames'][$i];
+                $rows .= $DLT->parseChunk($params['rowTpl'], $ph);
+                $ph['wrap'] = '';
+            }
+        }
+        $ph['wrap'] = $topRow . $rows;
+        $out = $DLT->parseChunk($params['ownerTPL'], $ph);
+
+        //echo $modx->parseDocumentSource(var_dump($result));
+        echo $modx->parseDocumentSource($out);
         die();
     }
-} elseif($modx->event->name==='OnWebPageInit') {
+} elseif ($modx->event->name === 'OnWebPageInit') {
     // Load Scripts and JS constants values
-    $jsConst = "const compareMaxCount = {$compareMaxCount};".PHP_EOL;
-    $jsConst .= 'const compareMaxCountMsg = "'.$compareMaxCountMsg['ru'].'";'.PHP_EOL;
-    $jsConst .= 'const compareActiveClass = "'.$compareActiveClass.'";'.PHP_EOL;
+    $jsConst = "const compareMaxCount = {$compareMaxCount};" . PHP_EOL;
+    $jsConst .= 'const compareMaxCountMsg = "' . $compareMaxCountMsg['ru'] . '";' . PHP_EOL;
+    $jsConst .= 'const compareActiveClass = "' . $compareActiveClass . '";' . PHP_EOL;
 
 
-    $modx->regClientScript('<script>'.$jsConst.'</script>');
+    $modx->regClientScript('<script>' . $jsConst . '</script>');
     $modx->regClientScript('assets/snippets/Compare/compare.js');
 }
